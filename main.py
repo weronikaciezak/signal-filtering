4963869f93852ebd2ff2fa09776be54c7bf55607
Weronika Ciężak <126866256+weronikaciezak@users.noreply.github.com>
2025-06-28 19:41:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,109 @@
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
+import tkinter as tk
+import numpy as np
 from PIL import Image, ImageTk
 import pandas as pd
-import tkinter as tk
 import matplotlib.pyplot as plt
+from scipy.signal import firwin, lfilter
 
 data = None
+filtered_signal = None
 org_sig_img = None
 new_sig_img = None
-filter = "low"
-sample_rate = 1000
-
-channels = []
-window = None
 
 def import_csv():
-    global data, channels, channel_var, channel_menu
+    global data, channel_var, channel_menu
 
     file = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
     if file:
-        data = pd.read_csv(file)
-        # channels = list(range(data.shape[1]))
-        # print(channels)
-        # channel_var.set(channels[0] if channels else 0)
-        #
-        # channel_menu.destroy()
-        # channel_menu_new = tk.OptionMenu(choice_frame, channel_var, *range(data.shape[1]))
-        # channel_menu_new.grid(row=1, column=2, padx=10, pady=5)
-        # channel_menu = channel_menu_new
+        data = pd.read_csv(file, delimiter=';', header=None)
+        channels = list(range(1, len(data.columns) + 1))
+        channel_var.set(channels[0] if channels else 0)
 
-        label_plik.config(text=f"Wybrano: {file}")
+        channel_menu.destroy()
+        channel_menu_new = tk.OptionMenu(choice_frame, channel_var, *channels)
+        channel_menu_new.grid(row=1, column=2, padx=10, pady=5)
+        channel_menu = channel_menu_new
+
+        label_file.config(text=f"Wybrano: {file}")
+        label_sample_rate.config(text=f"Liczba próbek: {data.shape[0]}, Liczba kanałów: {data.shape[1]}")
+
+def save_csv():
+    if filtered_signal is None:
+        messagebox.showerror("Błąd", "Najpierw wygeneruj sygnał.")
+        return
+
+    file = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
+    if file:
+        try:
+            filtered_signal.to_csv(file, index=False, sep=';')
+            messagebox.showinfo("Sukces", "Plik został zapisany pomyślnie.")
+        except Exception as e:
+            messagebox.showerror("Błąd", f"Nie można zapisać pliku: {e}")
 
 
-def plot_signal(signal, title):
+def plot_signal(signal, time, title):
     plt.figure(figsize=(8, 4))
-    plt.plot(signal)
-    # plt.title(title)
-    plt.xlabel("Próbki")
-    plt.ylabel("Amplituda")
-    plt.grid()
+    plt.plot(time, signal)
+    plt.xlabel('Czas [s]')
+    plt.ylabel('Amplituda')
+    plt.title('Oscylogram sygnału')
+    plt.grid(True)
     filename = title.replace(" ", "_").lower() + ".png"
     plt.savefig(filename)
+    plt.close()
 
-def original_signal():
+def filter_signal():
+    global filtered_signal
+
+    signal = data[channel_var.get() - 1]
+    filter_type = filter_kind.get()
+    window_type = window_var.get()
+
+    fs = float(sample_rate.get())
+    N = signal.shape[0]
+    t = np.arange(N) / fs
+    numtaps = 101
+
+    window_dict = {
+        "hamming": "hamming",
+        "hanning": "hann",
+        "blackman": "blackman",
+        "gauss": ("gaussian", 7),
+        "bartlett": "bartlett",
+        "prostokątne": "boxcar",
+    }
+
+    if filter_type == "dolnoprzepustowy":
+        cutoff = [fs * 0.1]
+        fir_coeff = firwin(numtaps, cutoff[0], window=window_dict[window_type], fs=fs)
+    elif filter_type == "gornoprzepustowy":
+        cutoff = [fs * 0.1]
+        fir_coeff = firwin(numtaps, cutoff[0], window=window_dict[window_type], fs=fs, pass_zero=False)
+    elif filter_type == "pasmowozaporowy":
+        cutoff = [fs * 0.2, fs * 0.3]
+        fir_coeff = firwin(numtaps, cutoff, window=window_dict[window_type], fs=fs, pass_zero=True)
+    else:
+        raise ValueError("Nieznany typ filtru.")
+
+    filtered_signal = lfilter(fir_coeff, 1.0, signal)
+    plot_signal(filtered_signal, t,"filtered signal")
+
+
+def generate_signal():
     global data, org_sig_img
 
-    plot_signal(data, "orginal signal")
+    if data is None and channel_var is not None:
+        messagebox.showerror("Błąd", "Najpierw wczytaj plik CSV.")
+        return
+
+    signal = data[channel_var.get() - 1]
+    N = signal.shape[0]
+    fs = float(sample_rate.get())
+
+    time = [i / fs for i in range(N)]
+
+    plot_signal(signal, time, "orginal signal")
     obraz = Image.open("orginal_signal.png")
     width, height = obraz.size
     obraz = obraz.resize((width // 2, height // 2))
@@ -52,7 +111,8 @@ def original_signal():
     label_obraz.config(image=org_sig_img)
     label_obraz_text.config(text="Oryginalny sygnał:")
 
-    obraz2 = Image.open("images.jpeg").resize((width // 2, height // 2))
+    filter_signal()
+    obraz2 = Image.open("filtered_signal.png").resize((width // 2, height // 2))
     new_sig_img = ImageTk.PhotoImage(obraz2)
     label_obraz2.config(image=new_sig_img)
     label_obraz2.image = new_sig_img
@@ -82,26 +142,31 @@ poz_x = (ekran_szer // 2) - (okno_szer // 2)
 poz_y = (ekran_wys // 2) - (okno_wys // 2)
 root.geometry(f"{okno_szer}x{okno_wys}+{poz_x}+{poz_y}")
 
-
 tk.Button(file_frame, text="Wczytaj plik CSV", command=import_csv).grid(row=0, column=0, padx=0, pady=0)
-label_plik = tk.Label(file_frame, text="Nie wybrano pliku")
-label_plik.grid(row=1, column=0)
+label_file = tk.Label(file_frame, text="Nie wybrano pliku")
+label_file.grid(row=1, column=0)
+label_sample_rate = tk.Label(file_frame, text="")
+label_sample_rate.grid(row=2, column=0)
 
-sample_rate_var = tk.StringVar(value="1000")
-filter_type = tk.StringVar(value="low")
-tk.Label(choice_frame, text="Wybierz częstotliwość:").grid(row=0, column=0, padx=10, pady=5)
-tk.Entry(choice_frame, textvariable=sample_rate_var).grid(row=1, column=0, padx=10, pady=5)
+sample_rate = tk.StringVar(value="1")
+tk.Label(choice_frame, text="Podaj próbkowanie:").grid(row=0, column=0, padx=10, pady=5)
+tk.Entry(choice_frame, textvariable=sample_rate).grid(row=1, column=0, padx=10, pady=5)
+
+filter_kind = tk.StringVar(value="dolnoprzepustowy")
 tk.Label(choice_frame, text="Wybierz rodzaj filtrowania:").grid(row=0, column=1, padx=10, pady=5)
-tk.OptionMenu(choice_frame, filter_type, "low", "high", "band").grid(row=1, column=1, padx=10, pady=5)
+tk.OptionMenu(choice_frame, filter_kind, "dolnoprzepustowy", "gornoprzepustowy", "pasmowozaporowy").grid(row=1, column=1, padx=10, pady=5)
 
+window_var = tk.StringVar(value="prostokątne")
+tk.Label(choice_frame, text="Wybierz okno:").grid(row=0, column=3, padx=10, pady=5)
+tk.OptionMenu(choice_frame, window_var, "hamming", "hanning", "blackman", "gauss", "bartlett", "prostokątne").grid(row=1, column=3, padx=10, pady=5)
 
-# channel_var = tk.IntVar()
-# channel_var.set(0)
-# channel_menu = tk.OptionMenu(choice_frame, channel_var, ())
-# channel_menu.grid(row=1, column=2, padx=10, pady=5)
-# tk.Label(choice_frame, text="Wybierz kanał:").grid(row=0, column=2, padx=10, pady=5)
+channel_var = tk.IntVar()
+channel_var.set(0)
+channel_menu = tk.OptionMenu(choice_frame, channel_var, ())
+channel_menu.grid(row=1, column=2, padx=10, pady=5)
+tk.Label(choice_frame, text="Wybierz kanał:").grid(row=0, column=2, padx=10, pady=5)
 
-tk.Button(ok_frame, text="Wygeneruj sygnał", command=original_signal).grid(row=0, column=0, padx=10, pady=5)
+tk.Button(ok_frame, text="WYGENERUJ SYGNAŁ", command=generate_signal).grid(row=0, column=0, padx=10, pady=5)
 
 label_obraz_text = tk.Label(photos_frame)
 label_obraz_text.grid(row=0, column=0, padx=10, pady=5)
@@ -115,8 +180,6 @@ label_obraz2_text.grid(row=0, column=1, padx=10, pady=5)
 label_obraz2 = tk.Label(photos_frame)
 label_obraz2.grid(row=1, column=1, padx=10, pady=5)
 
-# tk.Button(buttons_frame, text="Przycisk 1").grid(row=0, column=0, padx=5)
-# tk.Button(buttons_frame, text="Przycisk 2").grid(row=0, column=1, padx=5)
-# tk.Button(buttons_frame, text="Przycisk 3").grid(row=0, column=2, padx=5)
+tk.Button(ok_frame, text="Zapisz sygnał do pliku", command=save_csv).grid(row=0, column=1, padx=10, pady=5)
 
 root.mainloop()
